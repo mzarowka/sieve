315efe0c06fe831141d9e828f1c10a7ed7c7fa90
mzarowka <maurycy.zarczynski@outlook.com>
2023-04-10 16:48:42 -0700

    Change class to unit

--- R/utils.R
+++ R/utils.R
@@ -1,13 +1,21 @@
 ################################################################### Prepare data
 
-# Convert units between metric and phi, and vice versa
-convert_units <- function(size, class) {
+# #' Convert units between metric and phi
+#'
+#' @param size vector (numeric) of sizes.
+#' @param unit either "µm" or "phi" (character).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+convert_units <- function(size, unit) {
   # Convert to phi
-  if (class == "um") {
+  if (unit == "um") {
     size_phi <- -log2(size)
 
     # Convert to metric
-  } else if (class == "phi") {
+  } else if (unit == "phi") {
     size_um <- 2^(-size)
   }
 }
@@ -18,23 +26,23 @@ find_midpoint <- function(size) {
 }
 
 # Prepare tibble
-prepare_tibble <- function(data, class) {
-  if (class == "um") {
+prepare_tibble <- function(data, unit) {
+  if (unit == "um") {
     data <- data |>
       dplyr::mutate(
         size_mm = size / 1000,
-        size_phi = convert_units(size_mm, class = "um"),
+        size_phi = convert_units(size_mm, unit = "um"),
         midpoint_mm = find_midpoint(size_mm),
         midpoint_phi = find_midpoint(size_phi),
         .after = size_mm
       )
 
     return(data)
-  } else if (class == "phi") {
+  } else if (unit == "phi") {
     data <- data |>
       dplyr::mutate(
         size_phi = size,
-        size_mm = convert_units(size_phi, class = "phi"),
+        size_mm = convert_units(size_phi, unit = "phi"),
         midpoint_mm = find_midpoint(size_mm),
         midpoint_phi = find_midpoint(size_phi),
         .after = size
